a48f8f71a3e8b9cd7adfc2c2e27f681beb899f79
Mohamed Yasser <145772029+Mohamedyasser212@users.noreply.github.com>
2024-05-03 11:31:53 +0300

    Files upload

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+// var skils="conter";
+// if(skils== 'php'){
+//     console.log("learn backend");
+// }
+// else if(skils== 'js'){
+//     console.log("learn frontend");
+// }
+// else if(skils== 'c#'){
+//     console.log("learn desktop");
+// }
+// else {
+//     console.log("learn cooking")
+// }
+
+
+var skils='c#';
+
+switch(skils)
+{
+    case"php":
+    console.log("learn backend");
+    break;
+
+    case "js":
+    console.log('learn ftontend');
+    break;
+    case "c#":
+        console.log("learn desktop");
+        break;
+
+        default:
+            console.log('learn cooking');
+    }
